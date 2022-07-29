c4296a0353d8f76f08cb37807afb7a2fd9d06b07
Mehak Saini <mehaksaini053@gmail.com>
2022-07-29 22:58:14 +0530

    First Commit

---
+++ src/main/java/com/mehak/learning/codeapi/CodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.mehak.learning.codeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehak/learning/codeapi/CodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehak.learning.codeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
