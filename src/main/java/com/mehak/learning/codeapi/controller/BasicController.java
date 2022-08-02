bc746bfc24b892e0ad91b759d484e2e061dbc9cf
Mehak Saini <mehaksaini053@gmail.com>
2022-08-02 23:04:31 +0530

    Added armstrong API

--- src/main/java/com/mehak/learning/codeapi/controller/BasicController.java
+++ src/main/java/com/mehak/learning/codeapi/controller/BasicController.java
@@ -1,6 +1,7 @@
 package com.mehak.learning.codeapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class ApiController {
+@RequestMapping(path = "/basic")
+public class BasicController {
 
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "hello")
     public String sayHello(){
         return "Hello";
     }
 
-    @GetMapping(path = "/fib")
+    @GetMapping(path = "fib")
     public List<Integer> fibonacci(@RequestParam(name="num", defaultValue = "10") Integer num){
        int a = 0;
        int b = 1;
@@ -30,5 +32,21 @@ public class ApiController {
        }
 
        return fib;
+    }
+
+    @GetMapping(path = "armstrong")
+    public String ArmStrong(@RequestParam(name="num", defaultValue = "153") Integer num) {
+        int temp = num;
+        int sum = 0;
+        while (temp > 0) {
+            sum += (int) Math.pow((temp % 10), num.toString().length());
+            temp = temp / 10;
+        }
+        if (num.equals(sum)) {
+            return "Armstrong Number";
+        }
+        else{
+            return "Nope";
+        }
     }
 }
---
+++ src/main/java/com/mehak/learning/codeapi/controller/PatternController.java
@@ -0,0 +1,10 @@
+package com.mehak.learning.codeapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path ="/pattern")
+public class PatternController {
+
+}
